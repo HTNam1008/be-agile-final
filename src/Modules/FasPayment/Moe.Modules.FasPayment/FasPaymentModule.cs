21fc0d5fc1734b8a36fb000fdc0c99c56bb2ec73
c-Leroy Phan <c-leroy.phan@avepoint.com>
2026-06-22 16:15:36 +0700

    [FAS-L008] chore(integration): ensure compatibility with migration and test projects

--- src/Modules/FasPayment/Moe.Modules.FasPayment/FasPaymentModule.cs
+++ src/Modules/FasPayment/Moe.Modules.FasPayment/FasPaymentModule.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moe.Application.Abstractions.Modules;
 using Moe.Application.Abstractions.Persistence;
+using Moe.Modules.FasPayment.IGateway.Repositories;
+using Moe.Modules.FasPayment.Infrastructure.Repositories;
 
 namespace Moe.Modules.FasPayment;
 
@@ -10,6 +12,9 @@ public sealed class FasPaymentModule : IModule
 {
     public string Name => "FasPayment";
     public void AddServices(IServiceCollection services, IConfiguration configuration)
-        => services.AddSingleton<IModelConfigurationContributor, FasPaymentModelConfiguration>();
+    {
+        services.AddSingleton<IModelConfigurationContributor, FasPaymentModelConfiguration>();
+        services.AddScoped<IFasApplicationRepository, FasApplicationRepository>();
+    }
     public void MapEndpoints(IEndpointRouteBuilder endpoints) { }
 }
