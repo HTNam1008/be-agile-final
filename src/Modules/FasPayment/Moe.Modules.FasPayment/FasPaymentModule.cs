9a93c3f1d3cc442e2fbb720f3d9b3b12597a0ef7
c-Leroy Phan <c-leroy.phan@avepoint.com>
2026-06-22 16:49:52 +0700

    feat(FAS-S006): register FAS module CQRS handlers in Dependency Injection

--- src/Modules/FasPayment/Moe.Modules.FasPayment/FasPaymentModule.cs
+++ src/Modules/FasPayment/Moe.Modules.FasPayment/FasPaymentModule.cs
@@ -15,6 +15,11 @@ public sealed class FasPaymentModule : IModule
     {
         services.AddSingleton<IModelConfigurationContributor, FasPaymentModelConfiguration>();
         services.AddScoped<IFasApplicationRepository, FasApplicationRepository>();
+        
+        services.AddScoped<Moe.Application.Abstractions.Messaging.IQueryHandler<Moe.Modules.FasPayment.Application.Applications.GetSchemeApplications.GetSchemeApplicationsQuery, Moe.Modules.FasPayment.Application.Applications.GetSchemeApplications.GetSchemeApplicationsResponse>, Moe.Modules.FasPayment.Application.Applications.GetSchemeApplications.GetSchemeApplicationsHandler>();
+        services.AddScoped<Moe.Application.Abstractions.Messaging.IQueryHandler<Moe.Modules.FasPayment.Application.Applications.GetApplicationDetail.GetApplicationDetailQuery, Moe.Modules.FasPayment.Application.Applications.GetApplicationDetail.GetApplicationDetailResponse>, Moe.Modules.FasPayment.Application.Applications.GetApplicationDetail.GetApplicationDetailHandler>();
+        services.AddScoped<Moe.Application.Abstractions.Messaging.ICommandHandler<Moe.Modules.FasPayment.Application.Applications.Approve.ApproveApplicationCommand, Moe.Modules.FasPayment.Application.Applications.Approve.ApproveApplicationResponse>, Moe.Modules.FasPayment.Application.Applications.Approve.ApproveApplicationHandler>();
+        services.AddScoped<Moe.Application.Abstractions.Messaging.ICommandHandler<Moe.Modules.FasPayment.Application.Applications.Reject.RejectApplicationCommand, Moe.Modules.FasPayment.Application.Applications.Reject.RejectApplicationResponse>, Moe.Modules.FasPayment.Application.Applications.Reject.RejectApplicationHandler>();
     }
     public void MapEndpoints(IEndpointRouteBuilder endpoints) { }
 }
