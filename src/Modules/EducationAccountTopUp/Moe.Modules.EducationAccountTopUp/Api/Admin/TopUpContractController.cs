76c1601d9765b12a36c789fd368ba96be6c1f268
c-Leroy Phan <c-leroy.phan@avepoint.com>
2026-06-28 20:13:54 +0700

    [TOPUP-37] fix(topup): update contract controller route prefix to match conventions

--- src/Modules/EducationAccountTopUp/Moe.Modules.EducationAccountTopUp/Api/Admin/TopUpContractController.cs
+++ src/Modules/EducationAccountTopUp/Moe.Modules.EducationAccountTopUp/Api/Admin/TopUpContractController.cs
@@ -13,7 +13,7 @@ namespace Moe.Modules.EducationAccountTopUp.Api.Admin;
 
 [ApiController]
 [ApiVersion(1.0)]
-[Route("api/admin/v{version:apiVersion}/campaigns/{campaignId:long}/contracts")]
+[Route("api/admin/v{version:apiVersion}/top-up-campaigns/{campaignId:long}/contracts")]
 [Authorize(Policy = AuthorizationPolicies.AdminPortal)]
 [EnableCors("AdminCors")]
 public sealed class TopUpContractController(IQueryDispatcher queries) : ControllerBase
